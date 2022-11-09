705f6a8809c3c76d2fb58560b54e0e9c85d31618
Ashot Khanamiryan <a.khanamiryan@gmail.com>
2022-11-09 17:36:16 +0400

    init: added react redux feat: added store feat: added typed hooks for using with typescript feat: added Provider Wrapper for tests with redux

---
+++ src/store/store.ts
@@ -0,0 +1,30 @@
+import {
+  Action,
+  ThunkAction,
+  configureStore,
+  PreloadedState,
+  combineReducers,
+} from '@reduxjs/toolkit';
+
+export const reducersList = {};
+
+export const rootReducer = combineReducers(reducersList);
+
+const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export default setupStore;
