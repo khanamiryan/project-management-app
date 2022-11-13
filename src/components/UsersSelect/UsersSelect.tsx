b57a774efa0508891cf6cad9668d6a5890746e15
SiarheiHa <belposchta@gmail.com>
2022-11-13 12:08:59 +0300

    feat: add styles for UsersSelect, change mock-data

--- src/components/UsersSelect/UsersSelect.tsx
+++ src/components/UsersSelect/UsersSelect.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import Select, { MultiValue } from 'react-select';
+import { users } from 'mocks/mocks';
 import './UsersSelect.scss';
 
-const options = [
-  { value: '123', label: 'Chocolate' },
-  { value: '456', label: 'Strawberry' },
-  { value: '789', label: 'Vanilla' },
-];
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const UsersSelect = ({ onUserSelect }: { onUserSelect: (usersId: string[]) => void }) => {
+  const options = users.map(({ _id, name }) => {
+    return { value: _id, label: name };
+  });
+
   const onChange = (
-    value: MultiValue<{
+    arr: MultiValue<{
       value: string;
       label: string;
     }>
   ) => {
-    onUserSelect(value.reduce((acc: string[], current) => [...acc, current.value], []));
+    onUserSelect(arr.map(({ value }) => value));
   };
+
   return (
     <Select
       options={options}
       isMulti
-      onChange={(value) => onChange(value)}
+      isSearchable={true}
+      closeMenuOnSelect={false}
+      placeholder={'Share with'}
+      noOptionsMessage={() => 'no users'}
+      onChange={(arr) => onChange(arr)}
       className="users-select"
     />
   );
