eb6831493c61cfdb8d149a312fb00d7679f860c8
SiarheiHa <belposchta@gmail.com>
2022-11-29 00:48:18 +0300

    feat: fix styles

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,10 +1,47 @@
-import { Avatar, Box, Card, Divider, Grid, List, ListItem, Stack, Typography } from '@mui/material';
+import {
+  Avatar,
+  Box,
+  Card,
+  Divider,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Stack,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
 import './mainPage.scss';
+import { text } from 'stream/consumers';
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
+
+const developers = [
+  {
+    url: 'https://github.com/khanamiryan',
+    name: 'Ashot Khanamiryan',
+    imageSrc: 'https://avatars.githubusercontent.com/u/6542341?s=64&v=4',
+    role: 'team lead, developer',
+  },
+  {
+    url: 'https://github.com/utyfjs',
+    name: 'Henadzi Suhakou',
+    imageSrc: 'https://avatars.githubusercontent.com/u/79808010?s=64&v=4',
+    role: 'developer',
+  },
+  {
+    url: 'https://github.com/siarheiha',
+    name: 'Siarhei Hancharyk',
+    imageSrc: '/assets/jpg/siarhei.jpg',
+    role: 'developer',
+  },
+];
+
+const firstTextBlock =
+  '"The SuperBoards" is by far the best application for project management*. The unsurpassed functionality of our application for team work management will allow you to plan and control every stage of the project.';
+const footnote = '*According to the developers of this application.';
 
 export default function MainPage(): JSX.Element {
   return (
@@ -29,18 +66,20 @@ export default function MainPage(): JSX.Element {
         </Grid>
         <Grid
           item
-          xs={12}
-          sm={6}
-          md={6}
+          xs={8}
+          sm={4}
+          md={4}
           component="img"
           src="/assets/jpg/goal_800.jpg"
-          alt="goal image"
+          alt="goal"
+          height="auto"
         ></Grid>
       </Grid>
 
-      <Divider sx={{ mt: 2, mb: 2 }} />
+      <Divider />
 
       {/* section 2 */}
+
       <Grid
         container
         spacing={2}
@@ -48,6 +87,50 @@ export default function MainPage(): JSX.Element {
         alignItems={'center'}
         flexWrap="wrap-reverse"
       >
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={6}
+          component="img"
+          src="/assets/jpg/board_4_800.jpg"
+          alt="board"
+        ></Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <Typography
+            variant="h4"
+            component="p"
+            sx={{ textAlign: { xs: 'center', sm: 'justify' } }}
+          >
+            {firstTextBlock}
+          </Typography>
+          <Divider />
+          <Typography
+            variant="body1"
+            component="p"
+            color={'text.secondary'}
+            sx={{ textAlign: { xs: 'center', sm: 'justify' } }}
+          >
+            {footnote}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Divider />
+
+      {/* section 3 */}
+
+      <Grid container spacing={2} justifyContent="center" alignItems={'center'}>
+        <Grid item xs={12} sm={6} md={6}>
+          <Typography
+            variant="h4"
+            component="p"
+            sx={{ textAlign: { xs: 'center', sm: 'justify' } }}
+          >
+            Airtable is your organizations single source of truth. Its how teams, data, and
+            workflows are deeply connected by apps anyone can build.
+          </Typography>
+        </Grid>
         <Grid
           item
           xs={12}
@@ -56,50 +139,7 @@ export default function MainPage(): JSX.Element {
           component="img"
           src="/assets/jpg/board_3_800.jpg"
           alt="board"
-          // sx={{ maxWidth: { sm: '500px' } }}
         ></Grid>
-        <Grid item xs={12} sm={6} md={6}>
-          <Typography
-            variant="h4"
-            component="p"
-            sx={{ textAlign: { sx: 'center', sm: 'justify' } }}
-          >
-            Airtable is your organizations single source of truth. Its how teams, data, and
-            workflows are deeply connected by apps anyone can build.
-          </Typography>
-        </Grid>
-      </Grid>
-
-      <Divider sx={{ mt: 2, mb: 2 }} />
-
-      {/* section 3 */}
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        alignItems={'center'}
-        flexWrap="wrap-reverse"
-      >
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          md={6}
-          component="img"
-          src="/assets/jpg/board_2_800.jpg"
-          alt="board"
-          // sx={{ maxWidth: { sm: '500px' } }}
-        ></Grid>
-        <Grid item xs={12} sm={6} md={6}>
-          <Typography
-            variant="h4"
-            component="p"
-            sx={{ textAlign: { sx: 'center', sm: 'justify' } }}
-          >
-            Airtable is your organizations single source of truth. Its how teams, data, and
-            workflows are deeply connected by apps anyone can build.
-          </Typography>
-        </Grid>
       </Grid>
 
       <Divider sx={{ mt: 2, mb: 2 }} />
@@ -116,14 +156,14 @@ export default function MainPage(): JSX.Element {
           alignItems={'center'}
           flexWrap="wrap-reverse"
         >
-          {[1, 2, 3].map((el) => {
+          {developers.map(({ name, role, imageSrc, url }) => {
             return (
               <Grid
                 item
                 xs={12}
-                sm={12}
+                sm={4}
                 md={4}
-                key={el}
+                key={name}
                 sx={{ display: 'flex', justifyContent: 'center' }}
               >
                 <Card
@@ -136,27 +176,31 @@ export default function MainPage(): JSX.Element {
                   }}
                 >
                   <Avatar
-                    alt="Siarhei"
-                    src="/assets/jpg/siarhei.jpg"
+                    alt="photo"
+                    src={imageSrc}
                     sx={{ width: 200, height: 200 }}
                     variant="rounded"
                   />
-                  <Typography variant="h4">Siarhei</Typography>
+                  <Typography variant="h4">{name.split(' ')[0]}</Typography>
                   <Typography variant="h6" component={'p'} color="text.secondary">
-                    developer
+                    {role}
                   </Typography>
-                  <List>
-                    <ListItem>Feature</ListItem>
-                    <ListItem>Feature</ListItem>
-                    <ListItem>Feature</ListItem>
-                    <ListItem>Feature</ListItem>
-                    <ListItem>Feature</ListItem>
-                  </List>
                 </Card>
               </Grid>
             );
           })}
         </Grid>
+
+        <Divider />
+
+        <Typography variant="h6" component={'p'} textAlign="center">
+          This app was created as the final task of the{' '}
+          {
+            <Link href="https://rs.school/react/" target="_blank">
+              React course of the Rolling Scopes School.
+            </Link>
+          }
+        </Typography>
       </Box>
     </ThemeProvider>
   );
