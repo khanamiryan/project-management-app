19aad631ad737b8b1f6b4d5d2a24dc0b47bf5218
SiarheiHa <belposchta@gmail.com>
2022-12-04 20:23:03 +0300

    feat: add spinner and ErrorAlert

--- src/components/TaskSearchBlock/TaskSearchBlock.tsx
+++ src/components/TaskSearchBlock/TaskSearchBlock.tsx
@@ -1,7 +1,8 @@
-import { Box, List } from '@mui/material';
+import { Box, CircularProgress, List } from '@mui/material';
+import ErrorAlert from 'components/ErrorAlert/ErrorAlert';
 import Searchbar from 'components/Searchbar/Searchbar';
 import { useCurrentUser } from 'hooks/useCurrentUser';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGetTasksSetBySearchQuery } from 'services/board.api';
 import TaskListItem from './TaskListItem/TaskListItem';
 
@@ -9,7 +10,13 @@ const TaskSearchBlock = () => {
   const [inputValue, setInputValue] = useState('');
   const [searchValue, setSearchValue] = useState('');
   const { id: currentUserId } = useCurrentUser();
-  const { data: tasks, refetch } = useGetTasksSetBySearchQuery({
+  const {
+    data: tasks,
+    refetch,
+    isLoading,
+    isError,
+    isFetching,
+  } = useGetTasksSetBySearchQuery({
     userId: currentUserId,
     searchString: searchValue,
   });
@@ -43,10 +50,14 @@ const TaskSearchBlock = () => {
   return (
     <Box>
       <Searchbar value={inputValue} onChange={onSearchInputChange} onSubmit={searchTasks} />
-      <List>
-        {Boolean(userTasks.length) &&
-          userTasks.map((task) => <TaskListItem key={task._id} task={task} />)}
-      </List>
+      {isFetching && <CircularProgress size={80} />}
+      {isError && <ErrorAlert />}
+      {!isFetching && !isError && (
+        <List>
+          {Boolean(userTasks.length) &&
+            userTasks.map((task) => <TaskListItem key={task._id} task={task} />)}
+        </List>
+      )}
     </Box>
   );
 };
